267258a7ca67ca8b2a9723ffafc4568d9491006d
pedro Pablo Talavera Segura <pedrotalaveras@gmail.com>
2024-09-11 21:43:33 -0600

    modulo5

---
+++ 1-bundlers/parcel/dist/index.c43e4bbc.js
@@ -0,0 +1,2 @@
+let o=[];function n(n,c){o.push({product:n,quantity:c}),console.log(`Has agregado ${c} ${n}`)}console.log(o),console.log(n),n("shampoo",5),n("acondicionador",1),n("javon de cuerpo",3),console.log(o);
+//# sourceMappingURL=index.c43e4bbc.js.map
