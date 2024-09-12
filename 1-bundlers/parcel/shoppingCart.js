41e6f73b78be7c021925e31880514937e3e60e37
pedro Pablo Talavera Segura <pedrotalaveras@gmail.com>
2024-09-11 21:33:34 -0600

    modulo-5

---
+++ 1-bundlers/parcel/shoppingCart.js
@@ -0,0 +1,20 @@
+//carrito de compras
+let cart = [];
+//funcion que agregue objetos al carrito
+function addToCart(product, quantity){
+    //const item={
+    //    product: product,
+    //    quantity: quantity
+    //};
+    const item ={
+        product,
+        quantity
+    }
+    cart.push(item);
+    console.log(`Has agregado ${quantity} ${product}`);
+}
+
+export {
+    cart,
+    addToCart
+}
